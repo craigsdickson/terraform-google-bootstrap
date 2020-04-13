0ea5502d5d203d1cbdb8709516f757bc78392dd3
Craig S. Dickson <5379613+craigsdickson@users.noreply.github.com>
2020-04-13 13:09:49 +1000

    removed incorrect -i flag from base64 command line

--- modules/cloudbuild/cloudbuild_builder/entrypoint.bash
+++ modules/cloudbuild/cloudbuild_builder/entrypoint.bash
@@ -11,7 +11,7 @@ function activate-service-key() {
   mkdir -p $rootdir
   tmpdir=$(mktemp -d "$rootdir/servicekey.XXXXXXXX")
   trap "rm -rf $tmpdir" EXIT
-  echo ${GCLOUD_SERVICE_KEY} | base64 --decode -i > ${tmpdir}/gcloud-service-key.json
+  echo ${GCLOUD_SERVICE_KEY} | base64 --decode > ${tmpdir}/gcloud-service-key.json
   gcloud auth activate-service-account --key-file ${tmpdir}/gcloud-service-key.json --quiet
   get-active-account
 }
